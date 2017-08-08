2f5edaff93ffff8208e4b5699b43f7541921cd3b
Chris Kemp <chris.kemp3000@gmail.com>
2017-08-08 19:35:41 +0100

    Added brie and cheddar

--- hands-on-with-react-native/nottsjs2/App.js
+++ hands-on-with-react-native/nottsjs2/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+const brie = require('./images/brie.png');
+const cheddar = require('./images/cheddar.png');
+
+var styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'hsla(90,40%,50%,1)'
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center'
+  } 
+});
 
 export default class App extends React.Component {
   render() {
+    const s = {
+      fontSize: 30,
+      fontWeight: 'bold',
+      color: 'red'
+    };
     return (
       <View style={styles.container}>
-        <Text>This is our first React.Native app.!!</Text>
+
+        <Text style={s}>Brie!</Text>
+        <Image source={brie} />
+
+        <Text style={s}>Cheddar!</Text>
+        <Image source={cheddar} />
+
       </View>
     );
   }
